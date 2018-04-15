a1c16f43eb02f45f83de8f721ed443ea96129979
huhuapin <867840692@qq.com>
2018-04-15 19:06:13 +0800

    apidemo

---
+++ apidemo/app/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
+use Spatie\Permission\Traits\HasRoles;
+class User extends Authenticatable
+{
+    use Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+    ];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    public function generateToken()
+    {
+        $this->api_token = str_random(60);
+        $this->save();
+
+        return $this->api_token;
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}
