e82e8c74380f478ceac06fa4bd5651faa80cd4d4
huhuapin <867840692@qq.com>
2018-04-16 22:08:41 +0800

    微信登录获取token组件“

--- apidemo/app/Models/User.php
+++ apidemo/app/Models/User.php
@@ -16,6 +16,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'phone', 'email', 'password', 'introduction', 'avatar',
+        'weixin_openid', 'weixin_unionid'
     ];
     /**
      * The attributes that should be hidden for arrays.
